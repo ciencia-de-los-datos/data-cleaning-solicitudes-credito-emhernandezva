8cbcf4cae51768224b79dac525dcfdae59fbbc4c
emhernandezva <emhernandezva@unal.edu.co>
2022-10-05 12:21:10 -0500

    Intento 1

--- pregunta.py
+++ pregunta.py
@@ -12,9 +12,36 @@ import pandas as pd
 def clean_data():
 
     df = pd.read_csv("solicitudes_credito.csv", sep=";")
-
-    #
-    # Inserte su código aquí
-    #
-
+    df = df.drop('Unnamed: 0',axis=1)
+    
+    #mínusculas todos los datos tipo string
+    df['sexo'] = df['sexo'].str.lower()
+    df['tipo_de_emprendimiento'] = df['tipo_de_emprendimiento'].str.lower()
+    df['idea_negocio'] = df['idea_negocio'].str.lower()
+    df['barrio'] = df['barrio'].str.lower()
+    df['línea_credito'] = df['línea_credito'].str.lower()
+    
+    #eliminar espacios al principio y final
+    df['sexo'] = df['sexo'].str.strip()
+    df['tipo_de_emprendimiento'] = df['tipo_de_emprendimiento'].str.strip()
+    df['idea_negocio'] = df['idea_negocio'].str.strip()
+    df['barrio'] = df['barrio'].str.strip()
+    df['fecha_de_beneficio'] = df['fecha_de_beneficio'].str.strip()
+    df['línea_credito'] = df['línea_credito'].str.strip()
+    
+    #reemplazo de caracteres especiales dependiendo de la información de cada columna
+    df['idea_negocio'] = df['idea_negocio'].map(lambda x: x.replace('-',' ').replace('_',' ').strip())
+    df = df.dropna()
+    df['barrio'] = df['barrio'].map(lambda x: str(x).replace('-',' ').replace('_',' ').replace('bel¿n','belen').replace('nari¿o','nariño').strip())
+    df = df.dropna()
+    df['fecha_de_beneficio'] = df['fecha_de_beneficio'].map(lambda x: str(x).replace('-','/'))
+    df['fecha_de_beneficio'] = df['fecha_de_beneficio'].map(lambda x: x if len(x.split('/')[0])!=4 else f"{x.split('/')[2]}/{x.split('/')[1]}/{x.split('/')[0]}")
+    df['fecha_de_beneficio'] = df['fecha_de_beneficio'].map(lambda x: f"{x.split('/')[0].zfill(2)}/{x.split('/')[1].zfill(2)}/{x.split('/')[2].zfill(4)}")
+    df = df.dropna()
+    df['monto_del_credito'] = df['monto_del_credito'].map(lambda x: int(str(x).replace('$','').replace(',','').replace(' ','').replace('.00','')))
+    df = df.dropna()
+    df['línea_credito'] = df['línea_credito'].map(lambda x: x.replace('-','').replace('_','').replace(' ',''))
+    df = df.dropna()
+    df = df.drop_duplicates()
+    
     return df
